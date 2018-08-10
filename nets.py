c859458920e0c1546a81ebddf895f9f17cbe89a6
Garrett Ransom <g2ransom@gmail.com>
2018-08-10 18:07:44 -0400

    added sigmoid function and compute output function

--- nets.py
+++ nets.py
@@ -22,5 +22,25 @@ class NeuralNet():
 	'''Returns list of empty theta matrices based on dimensions'''
 	def initialize_theta(self, epsilon):
 		input_length = np.size(self.input_layer, 0)
-		layer_dims = [input_length] + self.hidden_layers
-		return [np.random.rand(layer_dims[i+1], layer_dims[i]) * ((2 * epsilon_init) - epsilon_init) for i in range(len(layer_dims[0:-1]))]
+		layer_dims = [input_length] + self.hidden_layers + [self.output_layer]
+		return [np.random.rand(layer_dims[i+1], layer_dims[i]) * ((2 * epsilon) - epsilon) for i in range(len(layer_dims[0:-1]))]
+
+	'''Returns np.ndarray of outputs using sigmoid function'''
+	def sigmoid(self, z):
+		assert isinstance(z, np.ndarray), 'input must be an np.ndarray'
+		return 1 / (1 + np.exp(-z))
+
+	'''Returns results of each layer of neural net until computing final output function'''
+	def compute_output(self, theta, n):
+		a = []
+		for i in range(0, n):
+			if i == 0:
+				temp_z = np.dot(theta[i], self.input_layer)
+				temp_a = self.sigmoid(-temp_z)
+				a.append(temp_a)
+			else:
+				temp_z = np.dot(theta[i], a[-1])
+				temp_a = self.sigmoid(-temp_z)
+				a.append(temp_a)
+			return a
+
