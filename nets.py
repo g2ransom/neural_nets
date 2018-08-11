bfe6bc136b11ad95fa98ca4a3e8454a7f5b91d18
Garrett Ransom <g2ransom@gmail.com>
2018-08-11 18:51:33 -0400

    added init function parameters and attributes and added cost function and sigmoid gradient function

--- nets.py
+++ nets.py
@@ -8,7 +8,7 @@ class NeuralNet():
 	output layer is the dimension of the output layer (based on the model)
 	'''
 
-	def __init__(self, input_layer, hidden_layers, output_layer):
+	def __init__(self, input_layer, hidden_layers, output_layer, y, epsilon):
 		if isinstance(input_layer, np.ndarray):
 			self.input_layer = input_layer
 		
@@ -18,12 +18,19 @@ class NeuralNet():
 		if isinstance(output_layer, int):
 			self.output_layer = output_layer
 
+		if isinstance(y, np.ndarray) and len(input_layer) == len(y):
+			self.y = y
 
-	'''Returns list of empty theta matrices based on dimensions'''
-	def initialize_theta(self, epsilon):
+		self.m = len(y)
+		self.epsilon = epsilon
+
 		input_length = np.size(self.input_layer, 0)
 		layer_dims = [input_length] + self.hidden_layers + [self.output_layer]
-		return [np.random.rand(layer_dims[i+1], layer_dims[i]+1) * ((2 * epsilon) - epsilon) for i in range(len(layer_dims[0:-1]))]
+		self.theta = [np.random.rand(layer_dims[i+1], layer_dims[i]+1) * ((2 * self.epsilon) - self.epsilon)
+						for i in range(len(layer_dims[0:-1]))]
+
+		self.theta_n = len(self.theta)
+
 
 	'''Returns np.ndarray of outputs using sigmoid function'''
 	def sigmoid(self, z):
@@ -31,9 +38,9 @@ class NeuralNet():
 		return 1 / (1 + np.exp(-z))
 
 	'''Returns results of each layer of neural net until computing final output function'''
-	def compute_output(self, theta, n):
+	def compute_output(self):
 		a = []
-		for i in range(0, n):
+		for i in range(0, self.theta_n):
 			if i == 0:
 				# add 1 to column of each input except output layer
 				X = self.input_layer
@@ -41,7 +48,7 @@ class NeuralNet():
 					X = np.insert(X, 0, 1, axis=0)
 				else:
 					X = np.insert(X, 0, 1, axis=1)
-				temp_z = np.dot(theta[i], X)
+				temp_z = np.dot(self.theta[i], X)
 				temp_a = self.sigmoid(-temp_z)
 				if temp_a.ndim == 1:
 					temp_a = np.insert(temp_a, 0, 1, axis=0)
@@ -49,11 +56,11 @@ class NeuralNet():
 					temp_a = np.insert(temp_a, 0, 1, axis=1)
 				a.append(temp_a)
 			elif i == (n-1):
-				temp_z = np.dot(theta[i], a[-1])
+				temp_z = np.dot(self.theta[i], a[-1])
 				temp_a = self.sigmoid(-temp_z)
 				a.append(temp_a)
 			else:
-				temp_z = np.dot(theta[i], a[-1])
+				temp_z = np.dot(self.theta[i], a[-1])
 				temp_a = self.sigmoid(-temp_z)
 				if temp_a.ndim == 1:
 					temp_a = np.insert(temp_a, 0, 1, axis=0)
@@ -62,3 +69,19 @@ class NeuralNet():
 				a.append(temp_a)
 		return a
 
+
+	def compute_cost(self, h, theta, lambda_value=0):
+		J_temp = (1 / self.m) * np.sum(np.multiply(-self.y, np.log(h)) - np.multiply((1 - y), np.log(1-h)))
+		if lambda_value == 0:
+			lambda_sum = 0
+		else:
+			lambda_sum = (lambda_value / (2 * self.m)) * sum([np.sum(np.power(theta[i], 2)) for i in range(len(theta)-1)])
+		return J_temp + lambda_sum
+
+
+	def cost_lambda(self, lambda_value):
+		pass
+
+
+	def sigmoid_gradient(self, z):
+		return np.multiply(self.sigmoid(-z), (1 - self.sigmoid(-z)))
