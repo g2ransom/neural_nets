edc61475daeba86cc03b714b465dab826b02985b
Garrett Ransom <g2ransom@gmail.com>
2018-08-20 07:53:28 -0400

    fixed dimension errors in class initiation and compute_output function

--- nets.py
+++ nets.py
@@ -10,7 +10,7 @@ class NeuralNet():
 
 
 	def __init__(self, input_layer, hidden_layers, output_layer, y, epsilon):
-		if isinstance(input_layer, np.ndarray):
+		if isinstance(input_layer, np.ndarray) and input_layer.ndim == 2:
 			self.input_layer = input_layer
 		
 		if isinstance(hidden_layers, list) and len(filter(lambda x: x <= 0, hidden_layers)) == 0:
@@ -27,7 +27,7 @@ class NeuralNet():
 		if epsilon > 0:
 			self.epsilon = epsilon
 
-		input_length = np.size(self.input_layer, 0)
+		input_length = np.size(self.input_layer, 1)
 		layer_dims = [input_length] + self.hidden_layers + [self.output_layer]
 		self.theta = [np.random.rand(layer_dims[i+1], layer_dims[i]+1) * ((2 * self.epsilon) - self.epsilon)
 						for i in range(len(layer_dims[0:-1]))]
@@ -53,6 +53,7 @@ class NeuralNet():
 						X = np.insert(X, 0, 1, axis=0)
 					else:
 						X = np.insert(X, 0, 1, axis=1)
+						X = np.transpose(X)
 					temp_z = np.dot(self.theta[i], X)
 					temp_a = self.sigmoid(-temp_z)
 					if temp_a.ndim == 1:
@@ -60,7 +61,7 @@ class NeuralNet():
 					else:
 						temp_a = np.insert(temp_a, 0, 1, axis=1)
 					a.append(temp_a)
-				elif i == (n-1):
+				elif i == (self.theta_n-1):
 					temp_z = np.dot(self.theta[i], a[-1])
 					temp_a = self.sigmoid(-temp_z)
 					a.append(temp_a)
@@ -87,42 +88,43 @@ class NeuralNet():
 
 
 	def sigmoid_gradient(self, z):
-		if isinstance(z, np.ndarray), 'z must be an np.ndarray':
+		if isinstance(z, np.ndarray):
 			return np.multiply(self.sigmoid(-z), (1 - self.sigmoid(-z)))
 
 
 	def backprop(self, learning_rate, iterations=1):
 		if isinstance(learning_rate, float):
-			theta_grad []
+			theta_grad = []
 			theta = self.theta
 			for z in range(self.theta_n):
 				theta_grad.append(np.zeros(theta[z].shape))
 			if  isinstance(iterations, int):
-				for epoch in iterations:
+				for epoch in range(0, iterations):
 					for i in range(self.m):
 						if self.input_layer.ndim == 1:
-							a = compute_cost(self.input_layer[i])
+							a = self.compute_output(self.input_layer[i])
 						else:
-							a = compute_cost(self.input_layer[i, :])
+							a = self.compute_output(self.input_layer[i, :])
 						if self.y.ndim == 1:
-							yy = self.y[t]
+							yy = self.y[i]
 						else:
-							yy = self.y[t, :]
+							yy = self.y[i, :]
 
 						'''compute the error for each layer'''
 						dels = []
-						for l in reversed(range(self.theta_n - 1)):
-							if l == (self.theta_n - 1):
+						for l in reversed(range(self.theta_n)):
+							if l == self.theta_n - 1:
 								error_l = a[-1] - yy
-								del_temp = error_l * sigmoid_gradient(np.dot(theta[l], a[l]))
+								del_temp = error_l * self.sigmoid_gradient(np.dot(theta[l], a[l]))
 								dels.append(del_temp)
 							else:
-								del_temp = np.multiply(np.dot(np.transpose(theta[l+1]), dels[-1]), sigmoid_gradient(np.dot(theta[l], a[l]))
+								del_temp = np.dot(np.transpose(theta[l+1]), dels[-1]) * self.sigmoid_gradient(np.dot(theta[l], a[l]))
 								dels.append(del_temp)
 
 							'''compute weight update'''
 							theta_grad[l] = theta_grad[l] + np.dot(del_temp, np.transpose(a[l-1]))
 							theta[l] = theta[l] - (learning_rate * theta_grad[l])
+		return theta
 			
 			
 
